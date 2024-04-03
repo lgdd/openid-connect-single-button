853f7710ab8d43213bb072b9cfc5fec52a6218e6
lgdd <louis-guillaume.durand@liferay.com>
2024-04-03 13:14:25 +0200

    refactor!: update to support latest changes
    Liferay changed how the form to sign in with an OpenID Connect Provider
    works. It used to send the provider name but now send the id instead.

--- src/open-id-connect/open-id-connect-single-button/main.js
+++ src/open-id-connect/open-id-connect-single-button/main.js
@@ -1,26 +1,49 @@
 const form = fragmentElement.querySelector("#_com_liferay_login_web_portlet_LoginPortlet_fm");
 const submitBtn = fragmentElement.querySelector("#_com_liferay_login_web_portlet_LoginPortlet_submit");
 const inputFormDate = fragmentElement.querySelector("#_com_liferay_login_web_portlet_LoginPortlet_formDate");
-const inputOIDCProviderName = fragmentElement.querySelector("#_com_liferay_login_web_portlet_LoginPortlet_OPEN_ID_CONNECT_PROVIDER_NAME");
+const inputOAuthClientEntryId = fragmentElement.querySelector("#_com_liferay_login_web_portlet_LoginPortlet_oAuthClientEntryId");
 const userMenu = fragmentElement.querySelector("div.user-menu");
-const isNotInEditionMode = !document.querySelector("body").classList.contains("company-site") && !document.querySelector("body").classList.contains("has-edit-mode-menu");
+const configurationHelper = fragmentElement.querySelector(".alert.alert-info");
+const configurationHelperList = fragmentElement.querySelector(".alert.alert-info ul");
+const isInEditionMode = document.querySelector("body").classList.contains("has-edit-mode-menu") || document.querySelector("body").classList.contains("portal-popup");
+
+inputOAuthClientEntryId.value = configuration.oAuthClientEntryId;
 
 inputFormDate.value = new Date().getTime();
-inputOIDCProviderName.value = configuration.oidcProviderName;
 
-submitBtn.onclick = function() {
-  form.submit();
+submitBtn.onclick = function () {
+	form.submit();
 }
 
-if(isNotInEditionMode) {
-	if(Liferay.ThemeDisplay.isSignedIn()) {
+if (isInEditionMode) {
+	fetchOAuthClientEntries();
+	form.classList.remove("hide");
+	userMenu.classList.remove("hide");
+	configurationHelper.classList.remove("hide");
+} else {
+	if (Liferay.ThemeDisplay.isSignedIn()) {
 		form.classList.add("hide");
 		userMenu.classList.remove("hide");
 	} else {
 		form.classList.remove("hide");
 		userMenu.classList.add("hide");
 	}
-} else {
-	form.classList.remove("hide");
-	userMenu.classList.remove("hide");
+}
+
+function fetchOAuthClientEntries() {
+	const payload = {
+		companyId: Liferay.ThemeDisplay.getCompanyId(),
+		authServerWellKnownURISuffix: 'openid-configuration'
+	};
+
+	Liferay.Service('/oauthclient.oauthcliententry/get-auth-server-well-known-uri-suffix-o-auth-client-entries', payload, function (list) {
+		for (let i = 0; i < list.length; i++) {
+			const infoJson = JSON.parse(list[i].infoJSON);
+			const clientName = infoJson.client_name;
+			const id = list[i].OAuthClientEntryId;
+			let li = document.createElement('li');
+			li.innerHTML = clientName + " (id=" + id + ")";
+			configurationHelperList.appendChild(li);
+		}
+	});
 }
